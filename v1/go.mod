b7ae499ceae3e158202261a82096b15914392ed3
andrtell <116459057+andrtell@users.noreply.github.com.>
2025-03-13 21:57:15 +0100

    WIP

--- v1/go.mod
+++ v1/go.mod
@@ -1,3 +1,3 @@
-module github.com/andrtell/tell.nu
+module github.com/andrtell/tell.nu/v1
 
 go 1.23.6
--- v1/main.go
+++ v1/main.go
@@ -1,40 +1,84 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
+
+type HttpError struct {
+	Status int
+	Err    error
+}
+
+func NewHttpError(status int, err error) *HttpError {
+	return &HttpError{Status: status, Err: err}
+}
+
+func (e *HttpError) Error() string {
+	return fmt.Sprintf("%d %s", e.Status, http.StatusText(e.Status))
+}
+
+func (e *HttpError) Unwrap() error {
+	return e.Err
+}
+
+type AppHandler func(http.ResponseWriter, *http.Request) error
+
+func (f AppHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := f(w, r); err != nil {
+		switch v := err.(type) {
+		case *HttpError:
+			http.Error(w, v.Error(), v.Status)
+		default:
+			http.Error(w, v.Error(), 500)
+		}
+	}
+}
 
 type App struct {
 	templates map[string]*template.Template
 }
 
-func (app *App) Init() {
+func (app *App) InitTemplates() {
 	app.templates = make(map[string]*template.Template)
-}
-
-func (app *App) ParseTemplates() {
 	home, err := template.ParseFiles(
 		"./templates/root.template",
 		"./templates/home.template",
 	)
 	if err != nil {
-		panic(err)
+		log.Fatalf("FATAL: %s", err)
 	}
 	app.templates["home"] = home
 }
 
-func (app *App) Home(w http.ResponseWriter, r *http.Request) {
-	app.templates["home"].ExecuteTemplate(w, "root.template", nil)
+func (app *App) Init() {
+	app.InitTemplates()
+}
+
+func (app *App) Home() AppHandler {
+	return func(w http.ResponseWriter, r *http.Request) error {
+		buf := new(bytes.Buffer)
+		// tpl.render(w, "home")
+		tpl := app.templates["home"]
+		err := tpl.ExecuteTemplate(buf, "root!.template", nil)
+		if err != nil {
+			return NewHttpError(500, err)
+		}
+		buf.WriteTo(w)
+		return nil
+	}
 }
 
 func main() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	app := &App{}
 	app.Init()
-	app.ParseTemplates()
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{$}", app.Home)
-	mux.HandleFunc("GET /index.html", app.Home)
+	mux.Handle("GET /{$}", app.Home())
+	mux.Handle("GET /index.html", app.Home())
 	mux.Handle("GET /static/", http.FileServer(http.Dir(".")))
 	http.ListenAndServe(":8000", mux)
 }
